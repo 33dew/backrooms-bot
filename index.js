f5193c666017524ec2fb1e55b735fb41e1fcd395
mosmo0220 <Mosmo2k@gmail.com>
2022-07-23 01:47:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Cygan!'))
 
 app.listen(port, () => console.log(`sample-expressjs app listening on port ${port}!`))
