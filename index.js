10d5d67015cf0a37652b0f4a81b6de9f6755a4c5
mosmo0220 <Mosmo2k@gmail.com>
2022-07-23 02:00:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,20 @@ const rest = new REST({ version: '10' }).setToken('MTAwMDE4ODI4Mjg0MTM1MDE0NA.Gz
     console.error(error);
   }
 })();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login('token');
