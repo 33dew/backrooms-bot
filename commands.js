50e41f00be90cb23c9e6cfa329582b7119ded744
mosmo0220 <Mosmo2k@gmail.com>
2022-07-23 02:16:11 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
-const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!')
@@ -9,6 +8,6 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken("MTAwMDE4ODI4Mjg0MTM1MDE0NA.Gzxnfy.TP_61e1CZHVWolRrQyBYPWYTpY-OomWq-mD4Es");
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands("1000188282841350144", "815333240595415090"), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
