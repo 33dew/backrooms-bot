f055dc3f7d79ced6a5804be9bb60f3a80898d224
kubek420 <kaudekbog@gmail.com>
2022-07-24 01:46:05 +0200

    Update deleteroom.js

--- interactions/buttons/deleteroom.js
+++ interactions/buttons/deleteroom.js
@@ -3,6 +3,7 @@ const { getRoom, updateRoom } = require('../../db/roomHandler')
 module.exports = {
     async execute(interaction) {
         const room = await getRoom(interaction.user.id)
+        room.settings = {...room.settings, isArchived: true}
         console.log(room)
         await interaction.reply({
             content: 'test'
