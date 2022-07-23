42cd0d4fe244f9a6e906f670e9f142b556f260b4
mosmo0220 <Mosmo2k@gmail.com>
2022-07-23 02:02:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord.js');
-
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-];
-
-const rest = new REST({ version: '10' }).setToken('MTAwMDE4ODI4Mjg0MTM1MDE0NA.Gzxnfy.TP_61e1CZHVWolRrQyBYPWYTpY-OomWq-mD4Es');
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -37,4 +13,4 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
-client.login('token');
+client.login('MTAwMDE4ODI4Mjg0MTM1MDE0NA.Gzxnfy.TP_61e1CZHVWolRrQyBYPWYTpY-OomWq-mD4Es');
