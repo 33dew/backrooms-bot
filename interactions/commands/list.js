bdb4e65e31be52aa04d50c84e842e59591209687
kubek420 <kaudekbog@gmail.com>
2022-07-24 05:25:58 +0200

    Added new feature

---
+++ interactions/commands/list.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js')
+const { getRoom } = require('../../db/controllers/roomController')
+const { isUserHasRoom } = require('../../db/roomHandler')
+
+module.exports = {
+    data:  new SlashCommandBuilder()
+        .setName("list")
+        .setDescription("List of users who have access to your section"),
+    async execute(interaction) {
+        let isUser = await isUserHasRoom(interaction.user.id)
+        if(!isUser) return interaction.reply({
+            content: 'You dont have room yet',
+            ephemeral: true
+        })
+        const room = await getRoom(interaction.user.id)
+        const users = room.map(user => `> <@${user}>`)
+        interaction.reply({
+            content: users.join('\n'),
+            ephemeral: true
+        });
+    }
+}
