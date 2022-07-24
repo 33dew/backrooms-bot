a0e291da434a788e5acbc511c01f333d3daac72d
kubek420 <kaudekbog@gmail.com>
2022-07-24 02:24:38 +0200

    Update deleteroom.js

--- interactions/buttons/deleteroom.js
+++ interactions/buttons/deleteroom.js
@@ -3,7 +3,7 @@ const { getRoom, archiveRoom } = require('../../db/roomHandler')
 module.exports = {
     async execute(interaction) {
         const room = await getRoom(interaction.user.id)
-        archiveRoom(interaction.user.id)
+        archiveRoom(`${interaction.user.id}`)
         const channel = await interaction.guild.channels.cache.get(room.category)
         channel.permissionOverwrites.edit(interaction.user, { ViewChannel: false })
         room.chats.forEach(e => {
