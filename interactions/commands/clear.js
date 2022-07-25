1a5f2fe58a92762a79dbb03761f4172136d2b09d
kubek420 <kaudekbog@gmail.com>
2022-07-25 10:30:12 +0200

    Update clear.js

--- interactions/commands/clear.js
+++ interactions/commands/clear.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js')
-const { addUser, isUserInRoom, collectRoom, isUserOwner } = require('../../db/roomHandler')
+const { addUser, isUserInRoom, isConfigChannel, collectRoom, isUserOwner } = require('../../db/roomHandler')
 
 module.exports = {
     data:  new SlashCommandBuilder()
@@ -13,6 +13,13 @@ module.exports = {
             .setRequired(true)
         ),
     async execute(interaction) {
+        let _isConfigChannel = await isConfigChannel(interaction.channel.id);
+        if (_isConfigChannel) {
+            return interaction.reply({
+                content: "Nie możesz użyć tej komendy w kanale, który jest konfiguracyjnym",
+                ephemeral: true
+            })
+        }
         let _isUserOwner = await isUserOwner(interaction.user.id ,interaction.channel.id);
         if (!_isUserOwner) {
             return interaction.reply({
