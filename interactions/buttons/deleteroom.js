b6a9535c1b11b7e37618c8fe95323b37e65fd85f
mosmo0220 <Mosmo2k@gmail.com>
2022-07-31 14:20:59 +0200

    Update deleteroom.js

--- interactions/buttons/deleteroom.js
+++ interactions/buttons/deleteroom.js
@@ -7,14 +7,14 @@ module.exports = {
         const channel = await interaction.guild.channels.cache.get(room.category)
         channel.permissionOverwrites.edit(interaction.user, { ViewChannel: false })
         room.chats.forEach(async e => {
-            const c = await interaction.guild.channels.cache.get(e)
+            const c = await interaction.guild.channels.cache.get(e.id)
             c.permissionOverwrites.edit(interaction.user, { ViewChannel: false })
         })
         room.users.forEach(async userid => {
             const user = interaction.guild.members.cache.get(userid)
             channel.permissionOverwrites.edit(user, { ViewChannel: false })
             room.chats.forEach(async e => {
-                const c = await interaction.guild.channels.cache.get(e)
+                const c = await interaction.guild.channels.cache.get(e.id)
                 c.permissionOverwrites.edit(user, { ViewChannel: false })
             })
         })
