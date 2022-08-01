48c2a7caf4e5f234921151e5cc69750ba0f00dc6
kubek420 <kaudekbog@gmail.com>
2022-08-01 08:30:32 +0200

    Update changesHandler.js

--- db/changesHandler.js
+++ db/changesHandler.js
@@ -5,24 +5,17 @@ module.exports = {
     getAndRemoveAllChanges().then((changesList) => {
     console.log(changesList);
       changesList.forEach((change) => {
-        const json = JSON.stringify(change);
-        const test = JSON.parse(json);
-        console.log(test.chatToRemove);
-        console.log(change.Room);
-        console.log(change.chatToRemove);
-        console.log(change._doc.chatToRemove);
-        console.log(Object.entries(change));
-        // await Promise.all(
-        //   change.chatToRemove.map(async (chat) => {
-        //     if (client.channels.cache.get(chat) != null) {
-        //       await client.channels.cache.get(chat).delete();
-        //     }
-        //   })
-        // );
-        // change.Room.forEach(async (room) => {
-        //     const categoryChannel = client.channels.cache.get(room.category);
-        //     console.log(categoryChannel);
-        // });
+        await Promise.all(
+          change._doc.chatToRemove.map(async (chat) => {
+            if (client.channels.cache.get(chat) != null) {
+              await client.channels.cache.get(chat).delete();
+            }
+          })
+        );
+        change.Room.forEach(async (room) => {
+            const categoryChannel = client.channels.cache.get(room.category);
+            console.log(categoryChannel);
+        });
       });
     });
   },
